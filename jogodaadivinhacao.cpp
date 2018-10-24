b1506a3aed79246126a8c11e3b6f04bc7e04789b
gafeol <gabfeol@gmail.com>
2018-10-24 08:42:30 -0300

    Aula 1

---
+++ jogodaadivinhacao.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+    cout << "**************************************" << endl;
+    cout << "* Bem-vindos ao jogo da adivinhação! *" << endl;
+    cout << "**************************************" << endl;
+
+    int numero_secreto = 42;
+    cout << "O número secreto é " << numero_secreto << ". Não conte para ninguém!" << endl;
+}
